8860f7cae290f7643aa8b617428976624fabccff
Froboz67 <kengel.ipa@gmail.com>
2024-09-12 10:22:32 -0400

    boilerplate for recipie project

---
+++ vue/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true
+  },
+  languageOptions: {
+    ecmaVersion: 2022
+  },
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:import/recommended'
+  ],
+  rules: {
+    'no-unused-vars': 'off'
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src']
+        ],
+        extensions: [".vue", ".js"]
+      }
+    },
+  },
+  // Needed for accessing .env from import.meta.env
+  'parserOptions': {
+    'ecmaVersion': 2022
+  }
+}
