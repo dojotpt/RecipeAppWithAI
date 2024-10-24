7620193d0fd2fc4d3dad2e5bffd6b03540ac0adc
Don Johnson <donjohnsontpt@gmail.com>
2024-10-24 18:34:03 -0400

    implemented recipe components

---
+++ vue/src/components/Recipe.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="recipe-container">
+      <div class="recipe-card">
+        <img class="recipe-image" :src="recipe.recipeImage" alt="picture of recipe"/>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+
+  </script>
+
